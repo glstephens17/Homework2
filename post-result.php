38c073bc5ac5fe49f2b57efe0693ac1896ec41be
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-09-18 04:41:28 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -1,14 +1,14 @@
 <?php
-$pageTitle = "Get result";
+$pageTitle = "Post result";
 include "view-header.php";
 ?>
-<h1>Get result</h1>
+<h1>Post Result</h1>
 <?php
 if (isset($_GET['my-name']))  {
 ?>
   <p>The value sent is:</p>
 <?php
-  echo $_GET['my-name'];
+  echo $_POST['my-name'];
 } else {
 ?>
   <p>Nothing sent to the page.</p>
